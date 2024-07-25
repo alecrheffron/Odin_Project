c36e73ae2a495c74e87849c183e7a216063b61f3
alecrheffron <140853421+alecrheffron@users.noreply.github.com>
2024-07-25 14:17:23 -0500

    Rockpaperscissors refactor

--- rock_paper_scissors_oop.rb
+++ rock_paper_scissors_oop.rb
@@ -1,57 +1,92 @@
+# frozen_string_literal: true
+
 class RockPaperScissors
-  def rock_paper_scissors()
-    ps = 0 # Player score
-    cs = 0 # Computer score
-    puts "What's your name?"
-    p1 = gets.chomp
-    puts "What do you want to play to: "
-    gw = gets.chomp.to_i # Score to win
-    loop do
-      while ps != gw && cs != gw
-        puts "Best #{gw} out of #{gw * 2 - 1}"
-        puts "#{p1}: #{ps}, Computer: #{cs}"
-        puts "\n"
-        puts "Rock, Paper, Scissors!"
-        gestures = ["Rock", "Paper", "Scissors"]
-        pg = gets.chomp.capitalize
-        cg = gestures[rand(0..2)]
-        if (pg == gestures[0] && cg == gestures[2]) || # Human Rock beats Scissors
-           (pg == gestures[1] && cg == gestures[0]) || # Human Paper beats Rock
-           (pg == gestures[2] && cg == gestures[1])    # Huamn Scissors beats Paper
-          puts "Ugh I picked #{cg}, you win"
-          puts "\n"
-          ps += 1
-        elsif (cg == gestures[0] && pg == gestures[2]) || # Puter Rock beats Scissors
-              (cg == gestures[1] && pg == gestures[0]) || # Puter Paper beats Rock
-              (cg == gestures[2] && pg == gestures[1])    # Puter Scissors beats Paper
-          puts "Ha! I picked #{cg}... you suck!!!"
-          puts "\n"
-          cs += 1
-        elsif pg == cg # Tie
-          puts "Darn, same here"
-          puts "\n"
-        else # Unaccepted input
-          puts "That's not what i asked"
-          puts "\n"
-        end
-      end
-      if ps == gw # Player win
-        puts "You got #{gw} so you win!"
-      elsif cs == gw # Computer win
-        puts "I got #{gw}, I win so suck it nerd!!"
-      end
-      puts "Wanna keep going?"
-      answer = gets.chomp.capitalize
-      if answer == "Yes" # Keep playing
-        gw += 1
-      elsif answer == "No" # Done playing
-        puts "Final score #{p1}: #{ps}, Computer: #{cs}"
-        break
-      else
-        puts "Whatever if you can't follow instructions then I quit"
-        puts "Final score #{p1}: #{ps}, Computer: #{cs}"
-        break
-      end
+  def initialize
+    @player_score = 0
+    @computer_score = 0
+    @gestures = ['Rock', 'Paper', 'Scissors']
+  end
+
+  def play
+    setup_game
+    until game_won?
+      display_scores
+      player_gesture = get_player_gesture
+      computer_gesture = @gestures.sample
+      determine_winner(player_gesture, computer_gesture)
+    end
+    announce_winner
+    play_again?
+  end
+
+  private
+
+  def setup_game
+    puts 'What\'s your name?'
+    @player_name = gets.chomp
+    puts 'What score do you want to play to?'
+    @winning_score = gets.chomp.to_i
+  end
+
+  def game_won?
+    @player_score == @winning_score || @computer_score == @winning_score
+  end
+
+  def display_scores
+    puts "Best #{@winning_score} out of #{@winning_score * 2 - 1}"
+    puts "#{@player_name}: #{@player_score}, Computer: #{@computer_score}\n\n"
+  end
+
+  def get_player_gesture
+    puts 'Rock, Paper, Scissors!'
+    gets.chomp.capitalize
+  end
+
+  def determine_winner(player_gesture, computer_gesture)
+    puts "Computer chose: #{computer_gesture}"
+    if winning_combination?(player_gesture, computer_gesture)
+      puts "Ugh, you win this round...\n\n"
+      @player_score += 1
+    elsif winning_combination?(computer_gesture, player_gesture)
+      puts "Computer wins.... you NERD!!\n\n"
+      @computer_score += 1
+    elsif player_gesture == computer_gesture
+      puts "That\'s what I picked...\n\n"
+    else
+      puts "That\s not what I asked...\n\n"
     end
   end
+
+  def winning_combination?(first, second)
+    (first == 'Rock' && second == 'Scissors') ||
+    (first == 'Paper' && second == 'Rock') ||
+    (first == 'Scissors' && second == 'Paper')
+  end
+
+  def announce_winner
+    if @player_score == @winning_score
+      puts "Well congrats #{@player_name}, you win..."
+    else
+      puts 'Computer wins again!! Down with humanity!!'
+    end
+    puts "Final score - #{@player_name}: #{@player_score}, Computer: #{@computer_score}"
+  end
+
+  def play_again?
+    puts 'Wanna play again? (Yes,No)'
+    answer = gets.chomp.capitalize
+    if answer == 'Yes'
+      raise_score
+      play
+    else
+      puts 'Fine, be a quitter'
+    end
+  end
+
+  def raise_score
+    @winning_score += 1
+  end
 end
+
+game = RockPaperScissors.new
+game.play
